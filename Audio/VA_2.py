00db5d0730d25f4cd321fb656bdde27205237633
Corbin <corbinr40@live.com>
2022-04-07 21:31:30 +0100

    Implemented previously added VA commands

---
+++ Audio/VA_2.py
@@ -0,0 +1,52 @@
+#PHASE 2
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+engine.setProperty('rate',200)
+engine.setProperty('voice', voices[1].id)
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
+except:
+    pass
+
+print(command)
+if 'play' in command:
+    song = command.replace('play', '')
+    talk('playing ' + song)
+    pywhatkit.playonyt(song)
+elif 'time' in command:
+    time = datetime.datetime.now().strftime('%I:%M %p')
+    talk('Current time is ' + time)
+elif 'who the heck is' in command:
+    person = command.replace('who the heck is', '')
+    info = wikipedia.summary(person, 1)
+    print(info)
+    talk(info)
+elif 'date' in command:
+    talk('sorry, I have a headache')
+elif 'are you single' in command:
+    talk('I am in a relationship with wifi')
+elif 'joke' in command:
+    talk(pyjokes.get_joke())
+else:
+    talk('Please say the command again.')
