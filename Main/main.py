9d4dbb4d10c93d07e6c9e2ba14175804cbc64e0b
Corbin <corbinr40@live.com>
2022-04-17 14:17:06 +0100

    Added live battery readings to main GUI

--- Main/main.py
+++ Main/main.py
@@ -6,9 +6,12 @@
 #When settings are wanted, it will call the settings page
 
 #from tkinter import Tk, Canvas, Frame, BOTH, Menu,
+import pstats
 from tkinter import *
 import datetime
 import time
+
+import platform
 
 #Modules for Face Detection
 #from tkinter import Tk, Canvas, Frame, BOTH
@@ -16,6 +19,57 @@ from multiprocessing import Process, Queue, Pipe
 import numpy.core.multiarray
 import cv2
 import threading
+
+#Current Power Level
+if('linux' in platform.system().lower()):
+    import smbus
+else:
+    pass
+
+# BUS VOLTAGE REGISTER (R)
+_REG_BUSVOLTAGE = 0x02
+
+# CALIBRATION REGISTER (R/W) #Required
+_REG_CALIBRATION = 0x05
+
+
+class BusVoltageRange:
+    """Constants for ``bus_voltage_range``"""
+    RANGE_16V = 0x00      # set bus voltage range to 16V
+    RANGE_32V = 0x01      # set bus voltage range to 32V (default)
+
+
+class INA219:
+    def __init__(self, i2c_bus=1, addr=0x40):
+        self.bus = smbus.SMBus(i2c_bus)
+        self.addr = addr
+
+        # Set chip to known config values to start #Required
+        self._cal_value = 0
+
+    def read(self, address):
+        data = self.bus.read_i2c_block_data(self.addr, address, 2)
+        return ((data[0] * 256) + data[1])
+
+    def write(self, address, data):
+        temp = [0, 0]
+        temp[1] = data & 0xFF
+        temp[0] = (data & 0xFF00) >> 8
+        self.bus.write_i2c_block_data(self.addr, address, temp)
+
+    def set_calibration_32V_2A(self):
+
+        #Required
+        self._cal_value = 4096
+
+        # Set Calibration register to 'Cal' calculated above #Required
+        self.write(_REG_CALIBRATION, self._cal_value)
+
+    def getBusVoltage_V(self):
+        self.write(_REG_CALIBRATION, self._cal_value)
+        self.read(_REG_BUSVOLTAGE)
+        return (self.read(_REG_BUSVOLTAGE) >> 3) * 0.004
+
 
 class Interface(Frame):
 
@@ -60,8 +114,9 @@ class Interface(Frame):
         self.currentTime()
 
         ##Battery Percentage Label
-        self.currentBatteryLabel = Label(menubarFrame, text="100%")
+        self.currentBatteryLabel = Label(menubarFrame)
         self.currentBatteryLabel.pack(anchor=E, side=RIGHT)
+        self.currentPower()
 
         ##Recording Audio / Camera
         audioOn = canvas.create_oval(620, 460, 640, 480, fill="#F85E2B", width=2)
@@ -82,6 +137,25 @@ class Interface(Frame):
         time = datetime.datetime.now().strftime('%H:%M')
         self.currentTimeLabel.config(text = time)
         self.currentTimeLabel.after(1000, self.currentTime)
+
+    def currentPower(self):
+        if('linux' in platform.system().lower()):
+
+            ina219 = INA219(addr=0x43)
+
+            bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
+            p = (bus_voltage - 3)/1.2*100
+            if(p > 100):
+                p = 100
+            if(p < 0):
+                p = 0
+
+            # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+            powerLevel = "{:3.0f}%".format(p)
+            self.currentBatteryLabel.config(text= powerLevel)
+            self.currentBatteryLabel.after(1000, self.currentPower)
+        else:
+            self.currentBatteryLabel.config(text="100%")
 
     #Face Detect Specific
     def createRect(self,x1, y1, x2, y2):
